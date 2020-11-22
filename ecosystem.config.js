27c60fb39f2b5062d4ddde2227ba7dc67807dfc5
Elliott-weeks <weeksi1999@outlook.com>
2020-11-22 14:26:17 +0000

    updated pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps : [{
       name: "kushling discord bot",
       script: "./index.js",
-      instances: "5",
+      instances: "1",
       env: {
         NODE_ENV: "development",
       },
