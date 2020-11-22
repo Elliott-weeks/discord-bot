5d41d41c57ce48a5187a709eb6dafc5fb878ea50
Elliott-weeks <weeksi1999@outlook.com>
2020-11-22 13:56:16 +0000

    production ready

--- index.js
+++ index.js
@@ -12,32 +12,44 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
     if (oldMember.id  == 779867519502254161 ) return
     console.log(oldMember.channelID);
     console.log(newMember.channelID);
-    let newUserChannel = newMember.voiceChannelID
-    let oldUserChannel = oldMember.voiceChannelID
-    if(newUserChannel === undefined){
+
+    if(oldMember.channelID == null && newMember.channelID){
+        let channel = client.channels.cache.get(newMember.channelID);
+        channel.join().then(connection => {
+            console.log("Joined channel")
+           const dispatcher = connection.play(require("path").join(__dirname, './hello.mp3'));
+           dispatcher.on('start', () => { 
+               dispatcher.setVolume(0.200);
+           }); 
+           dispatcher.on('error', (err) => console.log(err)); 
+          
+            dispatcher.on('finish', () => channel.leave());
+
+
+         }).catch(e => {
+           console.error(e);
+         })
+        
+
+    }
+    else if(oldMember.channelID && newMember.channelID == null){
         let channel = client.channels.cache.get(oldMember.channelID);
         channel.join().then(connection => {
-           // Yay, it worked!
-           console.log("Successfully connected.");
+            console.log("Joined channel")
            const dispatcher = connection.play(require("path").join(__dirname, './weakestLink.mp3'));
            dispatcher.on('start', () => { //not working
-               dispatcher.setVolume(0.70);
+               dispatcher.setVolume(0.200);
            }); 
            dispatcher.on('error', (err) => console.log(err)); //no errors
-           dispatcher.on('end', end => { //working fine
-
-        });
-
+          
+            dispatcher.on('finish', () => channel.leave());
 
          }).catch(e => {
            // Oh no, it errored! Let's log it to console :)
            console.error(e);
          })
-
-    
-
+    }
    
-     }
 })
 
 
