24dda4d9e0f7bc3ad93f5d212bdaa3017a5db809
Elliott-weeks <weeksi1999@outlook.com>
2020-11-22 14:13:13 +0000

    updated cluster

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps : [{
       name: "kushling discord bot",
       script: "./index.js",
-      instances: "max",
+      instances: "5",
       env: {
         NODE_ENV: "development",
       },
